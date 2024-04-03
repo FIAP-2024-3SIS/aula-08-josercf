2fa466afa90b4c479196a509ba7a16296732e269
Jose Romualdo <logonpflocal@fiap.com.br>
2024-04-03 20:38:11 -0300

    feat: add code to source control

---
+++ checkpointi/src/main/java/br/com/fiap/checkpointi/CheckpointiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.checkpointi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckpointiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckpointiApplication.class, args);
+	}
+
+}
---
+++ checkpointi/src/test/java/br/com/fiap/checkpointi/CheckpointiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.checkpointi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckpointiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
